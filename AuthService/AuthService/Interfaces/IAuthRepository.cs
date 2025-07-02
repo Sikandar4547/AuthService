82cf4aef235b0e6f46f3bfabf6bc137cc8d3a774
Sikandar <38713856+sikandarwazir@users.noreply.github.com>
2025-07-02 05:30:33 +0500

    working on register

---
+++ AuthService/AuthService/Interfaces/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.DTOs;
+
+namespace AuthService.Interfaces
+{
+    public interface IAuthRepository
+    {
+        string Register(Register register);
+        string Login(Login login);
+    }
+}
---
+++ AuthService/AuthService/Repositories/AuthRepository.cs
@@ -0,0 +1,22 @@
+﻿using AuthService.DTOs;
+using AuthService.Interfaces;
+
+namespace AuthService.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        public string Login(Login login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Register(Register register)
+        {
+            throw new NotImplementedException();
+        }
+        private string GenerateToken()
+        {
+            return string.Empty; // Placeholder for token generation logic
+        }
+    }
+}
